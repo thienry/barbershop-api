d5908a5368affa743f327244842f9d63a1a4f413
thiagomouraflima <77973330+thiagomouraflima@users.noreply.github.com>
2021-02-07 19:44:33 -0300

    Sequelize was confugured

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import chalk from 'chalk'
 import app from './app'
 
-app.listen(5000, () => console.log(chalk.bgGreen(chalk.whiteBright(`API has been started in port 5000!`))))
+app.listen(5000, () => console.log(chalk.bgGreen(chalk.whiteBright('API has been started in port 5000!'))))
 
 app.on('error', error => console.log(chalk.bgRed(chalk.whiteBright(error.message))))
